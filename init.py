518554b8f38f5e22e6642235343db5c6228198db
Jeffrey Wong <70869230+jeffwong97@users.noreply.github.com>
2024-12-08 17:51:08 -0500

    Turn off debug mode

--- init.py
+++ init.py
@@ -361,9 +361,6 @@ def logout():
 
 
 app.secret_key = 'some key that you will never guess'
-#Run the app on localhost port 5000
-#debug = True -> you don't have to restart flask
-#for changes to go through, TURN OFF FOR PRODUCTION
 if __name__ == "__main__":
-    app.run('127.0.0.1', 5000, debug = True)
+    app.run('127.0.0.1', 5000, debug = False)
 
